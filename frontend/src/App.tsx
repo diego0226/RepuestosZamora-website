04cf8f1c880b81f5dc7e9aa573e9e67c8ab60e23
diego0226 <zamoraarced@gmail.com>
2026-06-12 08:26:34 -0600

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
+
 import { ServicioList } from "./components/Servicio/ServicioList";
 import { RepuestoList } from "./components/Repuesto/RepuestoList";
 
 
 function App() {
-  const [vista, setVista] = useState<"servicios" | "repuestos">("servicios");
-
-  return (
-    <div>
-      <button onClick={() => setVista("servicios")}>Servicios</button>
-      <button onClick={() => setVista("repuestos")}>Repuestos</button>
-
-      {vista === "servicios" && <ServicioList />}
-      {vista === "repuestos" && <RepuestoList />}
-    </div>
-  );
+  
 }
 
 export default App;
